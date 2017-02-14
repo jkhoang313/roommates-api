7b65e82a701ef9c74b207348d8d01062c49e15e0
Vinny Martinez <vinny@vinnymartinez.com>
2017-02-14 17:05:07 -0500

    stuff to stash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  belongs_to :home, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213180214) do
+ActiveRecord::Schema.define(version: 20170214215014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.string   "address"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170213180214) do
     t.string   "user_name"
     t.string   "email"
     t.string   "password_digest"
+    t.integer  "home_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
