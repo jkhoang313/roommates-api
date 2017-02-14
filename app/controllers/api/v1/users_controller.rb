0c577e83f000efc4a2072f241e7d9c687bb83a24
Vinny Martinez <vinny@vinnymartinez.com>
2017-02-14 11:38:11 -0500

    add index and show to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -7,6 +12,11 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: "Error", status: 404
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   private
